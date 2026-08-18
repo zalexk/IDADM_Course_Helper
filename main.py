717057d40140875add72ce467c3226d1c6fd7350
alex <alexzsk-public@outlook.com>
2026-08-19 00:03:33 +0800

    Add link to introduce change in study scheme & contact to me

--- main.py
+++ main.py
@@ -52,9 +52,10 @@ if __name__ == "__main__":
         st.stop()
 
     if major_2nd: # Display only when 2nd major is selected
-        st.caption("\\* Data updated as of 17 Aug 2026")
+        st.caption("\\* Data updated as of 17 Aug 2026 ([See the changes](https://idadm.notion.site/study-scheme-change?source=copy_link))")
         st.caption("** This is unofficial. Please be aware that there may be mistakes.")
         st.caption("*** It is applicable to students admitted in 2025/26 from CUHK ONLY.")
+        st.caption("If you encounter any bugs, please contact me via [email](mailto:alex-zsk@link.cuhk.edu.hk) or [GitHub Issues](https://github.com/zalexk/IDADM_Course_Helper/issues).")
 
         ucore_tab, major_1_tab, major_2_tab, planner_tab = st.tabs(
             [
